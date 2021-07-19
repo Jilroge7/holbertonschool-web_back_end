a4c46918ce0b613a3e54d6e93fd3a0997208e9cd
Jilroge7 <1672@holbertonschool.com>
2021-07-19 21:21:01 +0000

    adding task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+type-annotated function.
+"""
+
+a = 1  # type: int
+pi = 3.14  # type: float
+i_understand_annotations = True  # type: bool
+school = "Holberton"  # type: str
