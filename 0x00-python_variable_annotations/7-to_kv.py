d73577023b59545e1a6543fb9db53f67c42df3dc
Jilroge7 <1672@holbertonschool.com>
2021-07-20 03:29:32 +0000

    adding task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+type-annotated function.
+"""
+from typing import List
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    string k and an int OR float v as arguments and returns a tuple
+    """
+    return (k, float(v ** 2))
