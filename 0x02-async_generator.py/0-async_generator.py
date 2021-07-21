354c3621f29470d1f76ef15150a84747a785246a
Jilroge7 <1672@holbertonschool.com>
2021-07-21 17:57:02 +0000

    adding task 0

---
+++ 0x02-async_generator.py/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+aync generator practice
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    coroutine loop 10x, asynchronously wait 1 second, then yield  random number
+    """
+    for i in range(10):
+        await ayncio.sleep(1)
+        yield random.uniform(0, 10)
