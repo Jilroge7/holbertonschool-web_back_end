d718aa7d8ddb29fbe6311a295ab81f696184cffb
Jilroge7 <1672@holbertonschool.com>
2021-07-21 18:04:34 +0000

    fixing typo

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,5 +12,5 @@ async def async_generator() -> Generator[float, None, None]:
     coroutine loop 10x, asynchronously wait 1 second, then yield  random number
     """
     for i in range(10):
-        await ayncio.sleep(1)
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
