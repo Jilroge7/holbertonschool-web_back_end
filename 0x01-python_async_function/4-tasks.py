4fbc7f1429681d41eb47e6823637ea91d256da35
Jilroge7 <1672@holbertonschool.com>
2021-07-21 17:26:00 +0000

    adding task 4 with edit

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,7 +3,7 @@
 Learning async/await and asyncio
 """
 import asyncio
-from typing import List
+from typing import List, Any
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
@@ -12,9 +12,9 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     takes in 2 int arguments, spawn wait_random n times specified max_delay
     """
     delay_list: List[float] = []
-    result_list: List[float] = []
+    result_list: List[Any] = []
     for _ in range(n):
-        delay_list.append(asyncio.create_task(task_wait_random(max_delay)))
+        delay_list.append(task_wait_random(max_delay))
     for delay_task in asyncio.as_completed(delay_list):
         result_list.append(await delay_task)
 
