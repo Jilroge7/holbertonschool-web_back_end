51b92e10901fcac9876727480f7847cf26f425a8
Jilroge7 <1672@holbertonschool.com>
2021-07-21 01:02:31 +0000

    adding task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Learning async/await and asyncio
+"""
+import asyncio
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    takes in 2 int arguments, spawn wait_random n times specified max_delay
+    """
+    delay_list: List[float] = []
+    result_list: List[float] = []
+    for _ in range(n):
+        delay_list.append(asyncio.create_task(task_wait_random(max_delay)))
+    for delay_task in asyncio.as_completed(delay_list):
+        result_list.append(await delay_task)
+
+    return (result_list)
