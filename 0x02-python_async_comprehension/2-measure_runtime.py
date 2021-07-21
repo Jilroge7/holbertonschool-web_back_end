1f02f7986d11b4d51efa5abb2cdce89be5bf70c1
Jilroge7 <1672@holbertonschool.com>
2021-07-21 19:11:26 +0000

    adding task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+async generator practice
+"""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_coroutine() -> float:
+    """
+    coroutine that will execute async_comprehension
+    """
+    start: float = time.time()
+    await asyncio.gather(*async_comprehension, 4)
+    end: float = time.time()
+
+    return (end - start)
