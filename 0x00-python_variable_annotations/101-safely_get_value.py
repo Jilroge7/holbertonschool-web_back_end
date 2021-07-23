a420d5d2b76778b3088035939bc03c9091cb51cd
Jilroge7 <1672@holbertonschool.com>
2021-07-23 18:37:22 +0000

    adding adv task 2

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+type-annotated function.
+"""
+from typing import Mapping, Union, Any, TypeVar
+
+
+def safely_get_value(
+            dct: Mapping,
+            key: Any,
+            default: Union[T, None] = None
+            ) -> Union[Any, T]:
+    """
+    annotated function that return values with appropriate types
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
