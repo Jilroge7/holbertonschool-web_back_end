45f26a2008467d2193d3db1d1af0c3206dbff49e
Jilroge7 <1672@holbertonschool.com>
2021-07-25 05:58:51 +0000

    adding initial files

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+0x03- caching, task 2 - lifo cache dictionary
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ child class inherits from basecaching, fifo"""
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ puts key in cache """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+            
+        if len(self.cache_data > self.MAX_ITEMS):
+            print("DISCARD: {}".format(key))
+        
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return (None)
+        value = self.cache_data.get(key)
+        return value
