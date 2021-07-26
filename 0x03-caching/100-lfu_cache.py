acf6703be1eedf8dbd2e7bc8b001e7ebc135106c
Jilroge7 <1672@holbertonschool.com>
2021-07-26 18:25:02 +0000

    adding partial adv task

---
+++ 0x03-caching/100-lfu_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+0x03- caching, task 4 - lfu cache dictionary
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ child class inherits from basecaching, fifo"""
+    def __init__(self):
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ puts key in cache """
+        if key and item is not None:
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
+            if len(self.cache_data) > self.MAX_ITEMS:
+                pop_key = self.cache_data.popitem(last=False)
+                print("DISCARD: {}".format(str(pop_key[0])))
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return (None)
+        self.cache_data.move_to_end(key)
+        value = self.cache_data.get(key)
+        return value
