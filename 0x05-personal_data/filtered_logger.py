8edb0655ef5558eb9572aca61d11c82fea95d763
Jilroge7 <1672@holbertonschool.com>
2021-08-02 01:57:21 +0000

    adding task 1

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -7,6 +7,28 @@ import re
 from typing import List
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        filter values in incoming log records
+        """
+        message = super().format(record)
+        formatted_message = filter_datum(self.fields, self.REDACTION,
+                                         message, self.SEPARATOR)
+        return formatted_message
+
+
 def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
