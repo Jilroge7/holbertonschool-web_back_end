ffaebdc80c7bb13f00dfb00831d772ccea320aca
Jilroge7 <1672@holbertonschool.com>
2021-08-11 23:21:32 +0000

    adding task2

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -7,10 +7,22 @@ from base64 import b64encode, b64decode
 from models.user import User
 from models.base import DATA
 from typing import TypeVar
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
     inherits from auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a session id
+        """
+        if user_id is None or type(user_id) != str:
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
