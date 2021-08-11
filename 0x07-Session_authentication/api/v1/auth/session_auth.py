f50ff6cdf0d1ac81a1dbc0516b39a8c50ad32591
Jilroge7 <1672@holbertonschool.com>
2021-08-11 23:30:23 +0000

    adding task 3

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -26,3 +26,12 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        get a user id from session id
+        """
+        if session_id is None or if type(session_id) != str:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
