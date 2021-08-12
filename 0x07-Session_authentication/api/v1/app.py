4cd2ff48fe0b0ded1eef1549046496b819addb6e
Jilroge7 <1672@holbertonschool.com>
2021-08-12 00:18:24 +0000

    adding task 5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -37,8 +37,8 @@ def before_req():
         'ap1/v1/auth_session/login/'
     ]
     if auth.require_auth(request.path, path_list) and auth is not None:
-        if (auth.authorization_header(request)) is None/
-        and auth.session_cookie(request) is None:
+        if (auth.authorization_header(request)) is None\
+                and auth.session_cookie(request) is None:
             abort(401)
         if (auth.current_user(request)) is None:
             abort(403)
