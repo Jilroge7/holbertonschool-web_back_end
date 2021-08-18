463d9784ea555e78421ccaad3a715f1daf7e85b5
Jilroge7 <1672@holbertonschool.com>
2021-08-18 04:26:50 +0000

    adding task 5

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -15,7 +15,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(email: str, password: str) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """
         register a user instance
         """
