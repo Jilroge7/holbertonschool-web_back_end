8b9b5d80620dbaa3e154a5a0ef39440f93ae1265
Jilroge7 <1672@holbertonschool.com>
2021-08-25 00:49:35 +0000

    removed extra parentheses from parameters

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -64,8 +64,8 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_repo_url.assert_called_once_with()
 
     @parameterized.expand([
-        ({"license": {"key": "my_license"}}), ("my_license"), (True),
-        ({"license": {"key": "other_license"}}), ("my_license"), (False)
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
     ])
     def test_has_license(self, repo, license_key, expected):
         """
