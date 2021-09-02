c75dbd8440f5af56eb579d7df04cdbe3273df61b
Jilroge7 <1672@holbertonschool.com>
2021-09-02 18:33:50 +0000

    edit to ass s in users

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- script to create table 'users' with attributes:
 -- id, email, name, and country. If the table exists it should not fail
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
