320b1154d64b3c1bd88dbaaae0f4079d417c384b
Jilroge7 <1672@holbertonschool.com>
2021-09-02 18:38:22 +0000

    edit to task 1

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 -- script to create table 'users' with attributes:
--- id, email, name, and country. If the table exists it should not fail
+-- id, email, name, and country: If the table exists it should not fail
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
