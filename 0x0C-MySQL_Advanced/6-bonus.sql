f60af7ac437057d4be0d362f1454cffe56e8b8af
Jilroge7 <1672@holbertonschool.com>
2021-09-03 04:26:45 +0000

    adding partial files

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,7 @@
+-- stored procedure 'AddBonus' takes 3 inputs:
+--user_id, project_name, score. Procedure will add a new correction to a student.
+DELIMITER //
+CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score FLOAT)
+BEGIN
+END//    
+DELIMITER ;
