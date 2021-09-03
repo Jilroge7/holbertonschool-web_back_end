ebe982cb118fde25c519f7eab71cdec5e016c855
Jilroge7 <1672@holbertonschool.com>
2021-09-03 04:43:01 +0000

    typo

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,10 +1,10 @@
 -- stored procedure 'AddBonus' takes 3 inputs:
 -- user_id, project_name, score. Procedure will add a new correction to a student.
 DELIMITER //
-CREATE PROCEDURE ComputerAverageScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
     UPDATE users
-    SET average = (SELECT AVG(score) FROM corrections as increased where increased.user_id=user_id)
+    SET average_score = (SELECT AVG(score) FROM corrections as increased where increased.user_id=user_id)
     WHERE id = user_id;
 END//    
 DELIMITER ;
