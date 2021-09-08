54c41fb796c26cdda6a9d743ec210330bf44ac46
Jilroge7 <1672@holbertonschool.com>
2021-09-08 02:40:46 +0000

    adding task 11

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+function that returns the list of schools having specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    return result of search string topic in collection
+    """
+    query_topic = {'topic': topic}
+    if mongo_collection:
+        return mongo_collection.find(query_topic)
+
+
+if __name__ == "__main__":
+    schools_by_topic(mongo_collection, topic)
