daae6c75f958803d703e21e0891c4091480a8649
Jilroge7 <1672@holbertonschool.com>
2021-09-10 02:44:19 +0000

    adding tasks 1-4, edit to task 0, got linter corrected

--- 0x0E-ES6_basic/0-constants.js
+++ 0x0E-ES6_basic/0-constants.js
@@ -1,19 +1,30 @@
 #!/usr/bin/node
+/**
+ * Displays a string.
+ * @returns {str} a string
+ */
 export function taskFirst () {
 
     const task = 'I prefer const when I can.';
-
 
     return task;
 
 }
 
+/**
+ * Displays a string.
+ * @returns {str} a string
+ */
 export function getLast () {
 
     return ' is okay';
 
 }
 
+/**
+ * Displays a string.
+ * @returns {str} a string
+ */
 export function taskNext () {
 
     let combination = 'But sometimes let';
