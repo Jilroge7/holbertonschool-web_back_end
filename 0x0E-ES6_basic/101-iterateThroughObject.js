73059a384df8e078ea3b81780a131fa544adf7d9
Jilroge7 <1672@holbertonschool.com>
2021-09-10 03:35:37 +0000

    adding empty bonus files for maybe some points

---
+++ 0x0E-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,6 @@
+export default function iterateThroughObject(reportWithIterator) {
+    return {
+        allEmployees: { ...reportWithIterator },
+        getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+    };
+}
