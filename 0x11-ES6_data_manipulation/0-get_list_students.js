e57bd0f34f4050ee4375bb5ea21a0ba0ab3b6da5
Jilroge7 <1672@holbertonschool.com>
2021-09-21 22:44:58 +0000

    typo in Columbia string

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 function getListStudents() {
   const student1 = ({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
-  const student2 = ({ id: 2, firstName: 'James', location: 'Colombia' });
+  const student2 = ({ id: 2, firstName: 'James', location: 'Columbia' });
   const student3 = ({ id: 5, firstName: 'Serena', location: 'San Francisco' });
   return [student1, student2, student3];
 }
