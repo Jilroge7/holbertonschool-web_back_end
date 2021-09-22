76b996461142ede636f89e57c94db50b61dbac32
Jilroge7 <1672@holbertonschool.com>
2021-09-22 03:12:22 +0000

    eslint fix

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,8 @@
 export default function updateStudentByCity(stuArray, city, newGrades) {
-    if (!Array.isArray(stuArray)) {
-        return [];
-      }
-    const stuByCity = stuArray.filter((x) => x.location === city);
-    return stuByCity;
+  if (!Array.isArray(stuArray)) {
+    return [];
+  }
+  const stuByCity = stuArray.filter((x) => x.location === city);
+  stuByCity.map((x) => x.newGrades);
+  return stuByCity;
 }
