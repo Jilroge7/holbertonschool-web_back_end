4ac5e4ffe0d57d472d8355823e32c4475303772a
Jilroge7 <1672@holbertonschool.com>
2021-09-22 03:19:28 +0000

    eslint

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw Error('Position outside range');
-c }
+  }
   const buffer = new ArrayBuffer(length);
   const int8View = new DataView(buffer);
   int8View.setInt8(position, value);
