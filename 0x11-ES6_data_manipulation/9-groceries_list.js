8e27ecbc95c26ce22928e4fee316cd22fe6b9877
Jilroge7 <1672@holbertonschool.com>
2021-09-22 03:53:27 +0000

    adding partial 8 and tsk 9

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const groceryItems = new Map();
+  groceryItems.set('Apples', 10);
+  groceryItems.set('Tomatoes', 10);
+  groceryItems.set('Pasta', 1);
+  groceryItems.set('Rice', 1);
+  groceryItems.set('Bananas', 5);
+  return groceryItems;
+}
