6d2340ac7dceec797f7bd7697851e60116406e44
Jilroge7 <1672@holbertonschool.com>
2021-09-27 19:34:45 +0000

    toying with eslint andlogic still

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -4,16 +4,17 @@ function countStudents(path) {
   try {
     const readAllFile = fs.readFileSync(path, 'utf8');
     const lines = readAllFile.split(/\r?\n/);
-  } catch (err) {
-    throw Error('Cannot load the database');
-  }
-  const stuCount = 0;
-  lines.forEach((line) => {
+    const stuCount = 0;
+    lines.forEach((line) => {
     stuCount += 1;
-  });
+    });
   const listFirstName = lines.map((line) => {
     line.split(',');
   });
+  } catch (err) {
+    throw Error('Cannot load the database');
+  }
+
   console.log(listFirstName);
   // const field = fs.readFileSync(path, 'utf8');
   // const fieldNum = fs.readFileSync(path, 'utf8');
