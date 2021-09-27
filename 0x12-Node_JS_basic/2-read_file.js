588410b44751b80d71d1a175a8dbc9c10447cce2
Jilroge7 <1672@holbertonschool.com>
2021-09-27 19:33:14 +0000

    toying with eslint andlogic still

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -3,11 +3,10 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const readAllFile = fs.readFileSync(path, 'utf8');
+    const lines = readAllFile.split(/\r?\n/);
   } catch (err) {
     throw Error('Cannot load the database');
   }
-
-  const lines = readAllFile.split(/\r?\n/);
   const stuCount = 0;
   lines.forEach((line) => {
     stuCount += 1;
@@ -16,9 +15,9 @@ function countStudents(path) {
     line.split(',');
   });
   console.log(listFirstName);
-  //const field = fs.readFileSync(path, 'utf8');
-  //const fieldNum = fs.readFileSync(path, 'utf8');
-  //console.log(`Number of students: ${stuCount}`);
-  //console.log(`Number of students in ${field}: ${fieldNum}. List: ${listFirstN}`)
+  // const field = fs.readFileSync(path, 'utf8');
+  // const fieldNum = fs.readFileSync(path, 'utf8');
+  // console.log(`Number of students: ${stuCount}`);
+  // console.log(`Number of students in ${field}: ${fieldNum}. List: ${listFirstN}`)
 }
 module.exports = countStudents;
