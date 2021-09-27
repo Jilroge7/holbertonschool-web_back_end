41e9bf204343cc0e54145905e7559d1ac9c30baf
Jilroge7 <1672@holbertonschool.com>
2021-09-27 23:09:57 +0000

    not quite right tsk 3

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,28 +1,28 @@
 const fs = require('fs');
 
-const countStudents = (path) => {return new Promise((resolve, reject) => {
-    fs.readFile(path, (err, readAllFile) => {
-      if (err) {
-        reject(err);
-        throw Error('Cannot load the database');
-      }
-      // resolve(readAllFile);
-      let lines = readAllFile.split(/\r?\n/);
-      lines.shift();
-      lines = lines.filter((line) => line !== '');
+const countStudents = (path) => { new Promise((resolve, reject) => {
+  fs.readFile(path, (err, readAllFile) => {
+    if (err) {
+      reject(err);
+      throw Error('Cannot load the database');
+    }
+    // resolve(readAllFile);
+    let lines = readAllFile.split(/\r?\n/);
+    lines.shift();
+    lines = lines.filter((line) => line !== '');
 
-      console.log(`Number of students: ${lines.length}`);
-      const csField = lines.filter((line) => line.endsWith('CS')).map((line) => {
-        const student = line.split(',');
-        return student[0];
-      });
-      console.log(`Number of students in CS: ${csField.length}. List: ${csField.join(', ')}`);
-      const sweField = lines.filter((line) => line.endsWith('SWE')).map((line) => {
-        const student = line.split(',');
-        return student[0];
-      });
-      console.log(`Number of students in SWE: ${sweField.length}. List: ${sweField.join(', ')}`);
-    })
+    console.log(`Number of students: ${lines.length}`);
+    const csField = lines.filter((line) => line.endsWith('CS')).map((line) => {
+      const student = line.split(',');
+      return student[0];
+    });
+    console.log(`Number of students in CS: ${csField.length}. List: ${csField.join(', ')}`);
+    const sweField = lines.filter((line) => line.endsWith('SWE')).map((line) => {
+      const student = line.split(',');
+      return student[0];
+    });
+    console.log(`Number of students in SWE: ${sweField.length}. List: ${sweField.join(', ')}`);
+  })
   },
   )};
 module.exports = countStudents;
