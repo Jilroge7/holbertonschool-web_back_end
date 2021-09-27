bc5fccbf78c2cec2c70046a8e5798ecd2e91e8f7
Jilroge7 <1672@holbertonschool.com>
2021-09-27 23:04:14 +0000

    eslint

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -5,19 +5,19 @@ function countStudents(path) {
     const readAllFile = fs.readFileSync(path, 'utf8');
     let lines = readAllFile.split(/\r?\n/);
     lines.shift();
-    lines = lines.filter(line => line !== '');
+    lines = lines.filter((line) => line !== '');
 
     console.log(`Number of students: ${lines.length}`);
-    
+
     const csField = lines.filter((line) => line.endsWith('CS')).map((line) => {
       const student = line.split(',');
       return student[0];
-      });
+    });
     console.log(`Number of students in CS: ${csField.length}. List: ${csField.join(', ')}`);
     const sweField = lines.filter((line) => line.endsWith('SWE')).map((line) => {
       const student = line.split(',');
       return student[0];
-      });
+    });
     console.log(`Number of students in SWE: ${sweField.length}. List: ${sweField.join(', ')}`);
   } catch (err) {
     throw Error('Cannot load the database');
